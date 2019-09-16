d1ff6cd8ed8c44b92b17c91713083b5c3371abb1
Onixaz <Onixaz@gmail.com>
2019-09-16 15:58:37 +0100

    More comments

--- alarm.py
+++ alarm.py
@@ -3,26 +3,37 @@ import RPi.GPIO as GPIO #pylint: disable=import-error
 import time
 import smtplib
 
-
+#https://support.google.com/mail/answer/185833?hl=en how to create app specific password for Gmail
 class Alarm:
 
-    def __init__(self, pin = 18):   
-        self.pin = pin
-        GPIO.setup(self.pin,GPIO.OUT)
+    def __init__(
+        self, pin = 18,
+        sender_email="YOUREMAIL@gmail.com", 
+        password="YOURREALORGENERATEDPASSWORD",
+        receiver_email="YOUREMAIL@ANY.COM"):
 
+        self.pin = pin
+        self.sender_email = sender_email
+        self.password = password
+        self.receiver_email = receiver_email
+        GPIO.setup(self.pin,GPIO.OUT)  
+    
     def beep(self):
         GPIO.output(self.pin, GPIO.HIGH)
-        time.sleep(0.5) # make sure this number is lower than the sensor_reading_delay in app.py
+        # make sure time.sleep() is lower than the sensor_reading_delay in app.py
+        time.sleep(0.5) 
         GPIO.output(self.pin, GPIO.LOW)
+    
     def cleanup(self):
         GPIO.cleanup()
 
+    #For this example we're using Python to connect to our Gmail account and send an email.
     def send_email(self):
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login("YOUREMAILd@gmail.com", "YOUREMAILPASSWORD")
+        server.login(self.sender_email, self.password)
         server.sendmail(
-            "YOUREMAIL@EXAMPLE.COM", 
-            "YOUREMAIL@EXAMPLE.COM", 
+            self.sender_email, 
+            self.receiver_email, 
             "!!! FIRE ALARM !!!")
         server.quit()
         
